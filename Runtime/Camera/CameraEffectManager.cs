af2ad9446966c68fe7d5aa1ca345cb47bd579ae9
Brevillo <oliver.beebe@digipen.edu>
2024-02-06 12:42:10 -0800

    removed camera shake type

--- Runtime/Camera/CameraEffectManager.cs
+++ Runtime/Camera/CameraEffectManager.cs
@@ -63,18 +63,6 @@ namespace OliverBeebe.UnityUtilities.Runtime.Camera {
         [Tooltip("The percent intensity of the shake over the duration.")]
         public AnimationCurve intensityCurve = AnimationCurve.Linear(0, 1, 1, 0);
 
-        [Tooltip("How the shake moves the camera.")]
-        public ShakeType shakeType = ShakeType.MoveToRandomPositions;
-
-        public enum ShakeType {
-
-            [Tooltip("Every frame the camera moves to a new position within the the amplitude radius.")]
-            NewPositionWithinRadiusEachFrame,
-
-            [Tooltip("The camera will move between random positions at a specified speed.")]
-            MoveToRandomPositions,
-        }
-
         [Tooltip("Duration it takes for the camera to move between shake positions.")]
         public float timeBetweenPositions = 0.02f;
 
@@ -105,98 +93,20 @@ namespace OliverBeebe.UnityUtilities.Runtime.Camera {
                       intensityPercent = profile.intensityCurve.Evaluate(timePercent),
                       amplitude        = intensityPercent * profile.amplitude;
 
-                Vector2 position = Vector2.zero;
-
-                switch (profile.shakeType) {
-
-                    case ShakeType.NewPositionWithinRadiusEachFrame:
-
-                        // choose random position
-                        position = Random.insideUnitCircle * amplitude;
-
-                        break;
-
-                    case ShakeType.MoveToRandomPositions:
-
-                        // choose new target position
-                        if (moveTimeRemaining <= 0) {
-                            prevTargetPosition = targetPosition;
-                            targetPosition = Random.insideUnitCircle * amplitude;
-                            moveTimeRemaining = profile.timeBetweenPositions;
-                        }
-
-                        // decrease timer and calculate position
-                        moveTimeRemaining -= dt;
-                        float movePercent = 1 - moveTimeRemaining / profile.timeBetweenPositions;
-                        position = Vector2.Lerp(prevTargetPosition, targetPosition, movePercent);
-
-                        break;
+                // choose new target position
+                if (moveTimeRemaining <= 0) {
+                    prevTargetPosition = targetPosition;
+                    targetPosition = Random.insideUnitCircle * amplitude;
+                    moveTimeRemaining = profile.timeBetweenPositions;
                 }
 
-                return position;
+                // decrease timer and calculate position
+                moveTimeRemaining -= dt;
+                float movePercent = profile.timeBetweenPositions == 0 ? 1 : 1 - moveTimeRemaining / profile.timeBetweenPositions;
+
+                return Vector2.Lerp(prevTargetPosition, targetPosition, movePercent);
             }
         }
-
-        #region Editor
-        #if UNITY_EDITOR
-
-        [CustomPropertyDrawer(typeof(CameraShakeProfile))]
-        private class CameraShakeProfilePropertyDrawer : PropertyDrawer {
-
-            private bool foldoutActive;
-
-            private readonly string[] properties = new[] {
-                nameof(amplitude),
-                nameof(duration),
-                nameof(unscaledTime),
-                nameof(intensityCurve),
-                nameof(shakeType),
-            };
-
-            private const string conditionalProp = nameof(timeBetweenPositions);
-
-            // found this here, search for float kIndentPerLevel: https://github.com/Unity-Technologies/UnityCsReference/blob/master/Editor/Mono/EditorGUI.cs
-            private const float indentWidth = 15f;
-
-            private CameraShakeProfile GetProfile(SerializedProperty property)
-                => fieldInfo.GetValue(property.serializedObject.targetObject) as CameraShakeProfile;
-
-            public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-
-                position.height = EditorGUIUtility.singleLineHeight;
-                foldoutActive = EditorGUI.Foldout(new(position) { xMin = position.xMin - indentWidth}, foldoutActive, label, true);
-
-                if (foldoutActive) {
-
-                    EditorGUI.indentLevel++;
-
-                    void Property(string name) {
-                        position.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
-                        EditorGUI.PropertyField(position, property.FindPropertyRelative(name));
-                    }
-
-                    foreach (var prop in properties)
-                        Property(prop);
-
-                    if (GetProfile(property).shakeType == ShakeType.MoveToRandomPositions)
-                        Property(conditionalProp);
-
-                    EditorGUI.indentLevel--;
-                }
-            }
-
-            public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
-
-                int lines = foldoutActive
-                    ? 7 + (GetProfile(property).shakeType == ShakeType.MoveToRandomPositions ? 1 : 0)
-                    : 1;
-
-                return EditorGUIUtility.singleLineHeight * lines + EditorGUIUtility.standardVerticalSpacing * (lines - 1);
-            }
-        }
-
-        #endif
-        #endregion
     }
 
     [System.Serializable]
